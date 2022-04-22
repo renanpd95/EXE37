52f8cecdf9ba244bd16176339cb10f5d6f928dce
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-22 15:44:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+a = int(input("Digite o valor das bases a: "))
+b = int(input("Digite o valor das bases b: "))
+h = int(input("Digite o valor da altura h: "))
+os.system('clear')
+result = (a+b)*h/2
+
+print("A área do trapezio é: ",result)
